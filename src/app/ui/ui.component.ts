89c240d4ad64aeb2677e98d67662aee550c620fc
MarianoOTech <marianosanro23@gmail.com>
2024-11-04 00:44:21 -0600

    add division ui tests

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { addition } from "../addition/addition"
 import { substraction } from "../substraction/substraction";
 import { multiplication } from '../multiplication/multiplication';
+import { division } from '../division/division';
 
 @Component({
   selector: 'app-ui',
@@ -33,7 +34,16 @@ export class UiComponent implements OnInit {
   }
 
   multiplication() {
+    let myresult = 0;
 
+    myresult = multiplication(this.operator1, this.operator2);
+    this.result = myresult;
   }
 
+  division() {
+    let myresult = 0;
+
+    myresult = division(this.operator1, this.operator2);
+    this.result = myresult;
+  }
 }
